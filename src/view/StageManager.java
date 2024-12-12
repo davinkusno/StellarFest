e9c02e102d45ab2a62d475a90817789bcb585a73
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-12 11:50:47 +0700

    fix: fix data deletion logic

--- src/view/StageManager.java
+++ src/view/StageManager.java
@@ -12,11 +12,13 @@ public class StageManager {
     private static volatile StageManager instance;
 
     private final Stage primaryStage;
-    private final Map<String, SFView> sceneMap;
+    private final Map<String, SFView> sfViewMap;
+    private final Map<Scene, SFView> sceneViewMap;
 
     public StageManager(Stage primaryStage) {
         this.primaryStage = primaryStage;
-        this.sceneMap = new HashMap<>();
+        this.sfViewMap = new HashMap<>();
+        this.sceneViewMap = new HashMap<>();
 
         instance = this;
     }
@@ -26,7 +28,8 @@ public class StageManager {
     }
 
     public void addScene(SFView scene) {
-        sceneMap.put(scene.getViewName(), scene);
+        sfViewMap.put(scene.getViewName(), scene);
+        sceneViewMap.put(scene.getScene(), scene);
     }
 
     public void switchScene(Class<? extends SFView> viewClass) {
@@ -35,16 +38,20 @@ public class StageManager {
     }
 
     public void switchScene(String name) {
-        SFView sfView = sceneMap.get(name);
+        SFView sfView = sfViewMap.get(name);
         if (sfView == null) {
             throw new IllegalArgumentException("No scene with name " + name);
+        }
+
+        Scene scene = primaryStage.getScene();
+        if (sceneViewMap.containsKey(scene)) {
+            sceneViewMap.get(scene).destroyView();
         }
 
         if (sfView instanceof Refreshable) {
             ((Refreshable) sfView).refreshData();
         }
 
-        sfView.destroyView();
         primaryStage.setScene(sfView.getScene());
         primaryStage.setTitle(sfView.getWindowTitle());
         primaryStage.show();
@@ -55,8 +62,8 @@ public class StageManager {
     }
 
     public static class SceneFactory {
-        private int width = 800;
-        private int height = 600;
+        private int width = 1000;
+        private int height = 800;
 
         private SceneFactory() {
         }
