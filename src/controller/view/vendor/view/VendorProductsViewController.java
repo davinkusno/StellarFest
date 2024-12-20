a6ba25c8e8cb32f79cd3e5fdb9946f64a550b3ce
Efran Nathanael <37508513+BadAccuracyID@users.noreply.github.com>
2024-12-20 21:00:17 +0700

    fix: fix vendor product view
    fix vendor product list view not properly updating

--- src/controller/view/vendor/view/VendorProductsViewController.java
+++ src/controller/view/vendor/view/VendorProductsViewController.java
@@ -1,9 +1,9 @@
 package controller.view.vendor.view;
 
+import controller.VendorProductController;
 import javafx.collections.ObservableList;
 import model.VendorProduct;
 import model.user.User;
-import model.user.impl.VendorUser;
 
 import java.util.List;
 
@@ -13,11 +13,7 @@ public class VendorProductsViewController {
         vendorProducts.clear();
 
         // Alternative implementation
-//        List<VendorProduct> products = VendorProductController.getForUser(user.getId());
-//        vendorProducts.addAll(products);
-
-        VendorUser vendorUser = (VendorUser) user;
-        List<VendorProduct> products = vendorUser.getVendorProducts().getValue();
+        List<VendorProduct> products = VendorProductController.getForUser(user.getId());
         vendorProducts.addAll(products);
     }
 
